9a53d518d8ad059d09c3b50e9f3b577c30ce5f0a
jmredfern-ar <46031172+jmredfern-ar@users.noreply.github.com>
2021-01-06 20:03:15 -0600

    Update Laser.py
    Added mouse control

--- Laser.py
+++ Laser.py
@@ -2,18 +2,20 @@ from settings import *
 
 #PROJECTILE CLASS
 class Laser(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, start_x, start_y, dest_x, dest_y):
         pygame.sprite.Sprite.__init__(self)
 
         #ADD LASER IMAGEs
+    
         self.lasers = [
-                       pygame.image.load(os.path.join(img_folder, "laser0.png")).convert(),
-                       pygame.image.load(os.path.join(img_folder, "laser1.png")).convert(),
-                       pygame.image.load(os.path.join(img_folder, "laser2.png")).convert(),
-                       pygame.image.load(os.path.join(img_folder, "laser3.png")).convert(),
-                       pygame.image.load(os.path.join(img_folder, "laser4.png")).convert(),
-                       pygame.image.load(os.path.join(img_folder, "laser5.png")).convert()
+                       pygame.image.load(os.path.join(img_folder, "robotLaser" + "0.png")).convert(),
+                       pygame.image.load(os.path.join(img_folder, "robotLaser" + "1.png")).convert(),
+                       pygame.image.load(os.path.join(img_folder, "robotLaser" + "2.png")).convert(),
+                       pygame.image.load(os.path.join(img_folder, "robotLaser" + "3.png")).convert(),
+                       pygame.image.load(os.path.join(img_folder, "robotLaser" + "4.png")).convert(),
+                       pygame.image.load(os.path.join(img_folder, "robotLaser" + "5.png")).convert()
                       ]
+        
         self.laser_count = 0
         
         self.image = self.lasers[self.laser_count]
@@ -22,12 +24,32 @@ class Laser(pygame.sprite.Sprite):
 
         #ESTABLISH RECT, STARTING POSITION
         self.rect = self.image.get_rect()
-        self.rect.bottom = y
-        self.rect.left = x
-        self.speedx = 10
+        self.rect.left = start_x
+        self.rect.bottom = start_y
+
+        #MAKE STARTING POINT MORE ACCURATE
+        self.floating_point_x = start_x
+        self.floating_point_y = start_y
+
+        #DIFFERENCE BTW START AND DEST PTS
+        x_diff = dest_x - start_x
+        y_diff = dest_y - start_y
+        angle = math.atan2(y_diff, x_diff)
+
+        #APPLY VELOCITY
+        self.speedx = 20
+        self.change_x = math.cos(angle) * self.speedx
+        self.change_y = math.sin(angle) * self.speedx
 
     def update(self):
-        self.rect.x += self.speedx
+
+        # The floating point x and y hold our more accurate location.
+        self.floating_point_y += self.change_y
+        self.floating_point_x += self.change_x
+ 
+        # The rect.x and rect.y are converted to integers.
+        self.rect.y = int(self.floating_point_y)
+        self.rect.x = int(self.floating_point_x)
 
         #TRANSITION BTW LASER PNGS
         self.image = self.lasers[self.laser_count]
@@ -39,5 +61,6 @@ class Laser(pygame.sprite.Sprite):
             self.laser_count = 0
 
         #DELETE LASER ONCE OFF SCREEN
-        if self.rect.left > WIDTH:
+        # If the bullet flies of the screen, get rid of it.
+        if self.rect.x < 0 or self.rect.x > WIDTH or self.rect.y < 0 or self.rect.y > HEIGHT:
             self.kill()
