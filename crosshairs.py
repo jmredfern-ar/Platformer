204cf58e5e51fef8c14e90c8a875116db9e48f04
jmredfern-ar <46031172+jmredfern-ar@users.noreply.github.com>
2021-02-08 10:10:28 -0600

    Update crosshairs.py
    Ripped out Timer() class code.

--- crosshairs.py
+++ crosshairs.py
@@ -13,34 +13,11 @@ class CrossHairs(pygame.sprite.Sprite):
         self.rect.y = 100
 
         self.pos = vec(0,0)
-        self.timer = Timer()
-        self.setCH = False
-
-        self.old_x = 0
-        self.old_y = 0
         
     def update(self):
         
         mouseState = pygame.mouse.get_pressed()
         pos = pygame.mouse.get_pos()
         
-        self.pos.x = pos[0]
-        self.pos.y = pos[1]
-
-        if mouseState[0] == 1 and self.setCH == False:
-            print("self.setCH", self.setCH)
-            print("self.timer.start()", self.timer.start())
-            
-            self.old_x = pos[0]
-            self.old_y = pos[1]
-
-            self.setCH = True
-            
-        if self.setCH and self.timer.getElapsedTime() < 2:
-            print("setting crosshairs") #set crosshairs for 2 secs
-            self.rect.centerx = self.old_x
-            self.rect.centery = self.old_y
-        else:
-            self.rect.centerx = self.pos.x
-            self.rect.centery = self.pos.y
-            self.setCH = False
+        self.rect.centerx = pos[0]
+        self.rect.centery = pos[1]
